b5d4a4eaa4fe1726bd5c35701e8c8349b2fbc8da
Jake <104261753+jakeft05@users.noreply.github.com>
2024-12-01 10:10:19 +0000

    Day 1

--- main.py
+++ main.py
@@ -1,2 +1,15 @@
 with open("input.txt", "r") as file:
   data = file.read()
+
+list1 = []
+list2 = []
+similarity = 0
+
+for line in data.split("\n"):
+  list1.append(int(line.split("   ")[0]))
+  list2.append(int(line.split("   ")[1]))
+
+for i in list1:
+  similarity += i*list2.count(i)
+
+print(str(similarity))
