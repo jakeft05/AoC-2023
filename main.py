4954502747112fa03e807727034f114f8430c126
Jake <104261753+jakeft05@users.noreply.github.com>
2024-12-02 17:00:40 +0000

    Part 1 completion

--- main.py
+++ main.py
@@ -1,15 +1,34 @@
 with open("input.txt", "r") as file:
   data = file.read()
 
-list1 = []
-list2 = []
-similarity = 0
+safeFloors = 0
 
-for line in data.split("\n"):
-  list1.append(int(line.split("   ")[0]))
-  list2.append(int(line.split("   ")[1]))
+for report in data.split("\n"):
+  levels = report.split(" ")
+  safe = True
 
-for i in list1:
-  similarity += i*list2.count(i)
+  # Increasing
+  if int(levels[0]) < int(levels[1]):
+    previous = int(levels[0]) - 1
+    for level in levels:
+      if int(level) <= previous + 3 and int(level) > previous and safe is True:
+        previous = int(level)
+      else:
+        safe = False
 
-print(str(similarity))
+  # Decreasing  
+  elif int(levels[0]) > int(levels[1]):
+    previous = int(levels[0]) + 1
+    for level in levels:
+      if int(level) >= previous - 3 and int(level) < previous and safe is True:
+        previous = int(level)
+      else:
+        safe = False
+
+  else:
+    safe = False
+
+  if safe is True:
+    safeFloors += 1
+
+print(str(safeFloors))
